93495bf5744547d1ba2c97fb692d3be98f496cb1
uzair <developeruzair401@gmail.com>
2025-04-19 23:50:40 +0500

    Fixed Endpoints issue in swagger UI

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import ShortURL
 from .serializers import ShortURLSerializer
+from drf_yasg.utils import swagger_auto_schema
 
+
+@swagger_auto_schema(methods=['post'], request_body=ShortURLSerializer)
 @api_view(['POST'])
 def shorten_url(request):
     serializer = ShortURLSerializer(data=request.data)
@@ -13,7 +16,9 @@ def shorten_url(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 #Getting original URL
+@swagger_auto_schema(methods=['get'])
 @api_view(['GET'])
 def redirect_url(request, short_url):
     url_obj = ShortURL.objects.filter(short_url=short_url).first()
@@ -30,6 +35,7 @@ def redirect_url(request, short_url):
 
 
 #Updating the URL
+@swagger_auto_schema(methods=['post'], request_body=ShortURLSerializer)
 @api_view(['PUT'])
 def update_url(request, short_url):
     url_obj = ShortURL.objects.filter(short_url=short_url).first()
@@ -42,6 +48,7 @@ def update_url(request, short_url):
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@swagger_auto_schema(methods=['delete'])
 @api_view(['DELETE'])
 def delete_url(request, short_url):
     url_obj = ShortURL.objects.filter(short_url=short_url).first()
@@ -53,6 +60,7 @@ def delete_url(request, short_url):
 
 
 #Getting Total statistics of the URL
+@swagger_auto_schema(methods=['get'])
 @api_view(['GET'])
 def statistics(request, short_url):
     url_obj = ShortURL.objects.filter(short_url=short_url).first()
