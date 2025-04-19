9547a2aa26f7f84455bb617ae88509d6ac63eba8
uzair <developeruzair401@gmail.com>
2025-04-19 20:22:53 +0500

    Model setup for urls

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class ShortUrl(models.Model):
+    short_url = models.CharField(max_length=10, unique=True)
+    original_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hit_count = models.IntegerField(default=0)
+
+    #in case the user didn't provide shortcode 
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = str(uuid.uuid4())[:8]
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.short_url} -> {self.original_url} ({self.hit_count} hits)"
+    
+    
+    
