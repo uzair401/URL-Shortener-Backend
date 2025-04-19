552c14966566df4006043d0ba4f80120d1a541f0
uzair <developeruzair401@gmail.com>
2025-04-19 21:54:40 +0500

    Swagger UI Implementation for Interactive UI

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.shorten_url, name='shorten_url'),
     path('<str:short_url>/', views.redirect_url, name='redirect_url'),
--- url_shortener_API/urls.py
+++ url_shortener_API/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="URL Shortener APP",
+      default_version='v1',
+      description="URL Shortener APP",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shorten_url/', include('main_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
